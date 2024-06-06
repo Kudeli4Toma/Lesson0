36298d714f0ab707e64c62018f14160d972c83b8
Kudeli4Toma <kudeli4toma@gmail.com>
2024-06-06 19:53:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+a = "piano teacher"
+print(len(a))
+
+first = 3
+second = 4
+summa =first+second
+diff =first-second
+print(summa,",", diff)
+
+first = 4
+second = 15
+third = 23
+mean =(first+second+third)/3
+print(mean)
+
+a = 5
+b = 7
+c = 8
+f = (((a*b) + (a*c)) ** 3 // 2)
+print(f)
+
+first_string = "Понедельник"
+second_string = "Вторник"
+print(first_string,",", second_string)
+
+
+

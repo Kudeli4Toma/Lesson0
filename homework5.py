8d9dd5f83e804dd8ffee21da0c927705e82d0b53
Kudeli4Toma <kudeli4toma@gmail.com>
2024-07-06 22:54:57 +0300

    Add files via upload
    Tuples. Immutable and mutable objects.

---
+++ homework5.py
@@ -0,0 +1,10 @@
+immutable_var = 11, 'Stefaniia', 2020
+print(immutable_var)
+#immutable_var[1] = 'Tamara'
+
+mutable_list = [2, 12, 19, 'sea']
+print(mutable_list)
+mutable_list[3] = 'ocean'
+print(mutable_list)
+
+

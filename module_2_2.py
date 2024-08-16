25cace87683d70d6613b0a3351af5a184f2817ae
Kudeli4Toma <kudeli4toma@gmail.com>
2024-08-16 14:04:20 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,17 +1,15 @@
 first = 34
 second = 46
 third = 12
+
 print(first)
 print(second)
 print(third)
 
-
-
 if first == second and second == third and third == first:
     print('3')
-elif first <= second and second >= third and third >= first:
+elif first == second or first == third or second == third:
     print("2")
-elif first != second and second != third and third != first:
+else:
     print("0")
-
 
